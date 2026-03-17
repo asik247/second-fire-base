a459f5341656b60b3089684b657413b7906a6167
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-17 15:03:24 +0600

    google sign up done

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 // Google Provider code here now;
-import { GoogleAuthProvider } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from '../../Firebase/firebase.init';
 const googleProvider = new GoogleAuthProvider();
-// handle sing up with google code here now;
-const handleGoogle = ()=>{
-    console.log("handle google btn clicked");
-}
+
 
 const Login = () => {
+    const [users, setUsers] = useState(null);
+    // handle sing up with google code here now;
+    const handleGoogle = () => {
+        console.log("handle google btn clicked");
+        signInWithPopup(auth, googleProvider)
+            .then(res => {
+                console.log(res.user);
+                setUsers(res.user)
+            }).catch(error => {
+                console.log(error);
+            })
+    }
     return (
         <div>
             <h1 className='text-2xl font-bold text-green-600 text-center mb-10'>Login Page</h1>
@@ -15,6 +25,13 @@ const Login = () => {
                 <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
                 Sign up with Google
             </button>
+            {users &&
+                <div className='border-2 border-gray-400 p-4 mt-10'>
+                    <h1 className='font-bold text-2xl'>Name: {users.displayName}</h1>
+                    <p>E-mail: {users.email}</p>
+                    <img src={users.photoURL} alt="" />
+                </div>
+            }
         </div>
     );
 };
