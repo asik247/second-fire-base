581e5ce73199d16575badb37f025490571ab7f24
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-17 14:18:38 +0600

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1 className='text-2xl font-bold text-red-500'>Helllo</h1>
+      <h1 className='text-2xl font-bold text-red-500 text-center'>Helllo</h1>
     </div>
   );
 };
