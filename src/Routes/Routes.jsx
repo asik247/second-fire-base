3f45524400ae48e0500f06e4e9e4ea0d9410db6e
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-17 14:21:30 +0600

    navbar update

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,8 +1,16 @@
 import { createBrowserRouter } from "react-router";
+import Root from "../Root/Root";
+import Home from "../Components/Home/Home";
+import Login from "../Components/Login/Login";
 
 const router = createBrowserRouter([
     {
         path:'/',
+        Component:Root,
+        children:[
+            {index:true,Component:Home},
+            {path:'login',Component:Login},
+        ]
 
     }
 ])
